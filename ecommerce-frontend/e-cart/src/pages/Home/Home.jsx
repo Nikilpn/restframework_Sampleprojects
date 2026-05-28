aaf38cfe1cda34c53f19575948559f160210949e
Nikilpn <nikhilpnnick1234@gmail.com>
2026-05-28 20:32:57 +0530

    added pagination

--- ecommerce-frontend/e-cart/src/pages/Home/Home.jsx
+++ ecommerce-frontend/e-cart/src/pages/Home/Home.jsx
@@ -1,19 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import './Home.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAllProducts } from '../../redux/productsSlice'
+import { faBackward, faForward } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Home() {
   const dispatch = useDispatch()
   const { allproducts, loading, error } = useSelector(state => state.productsReducer)
+
+  const productsPerPage=8
+  const totalPages=Math.ceil(allproducts.length/productsPerPage)
+  const [currentPage,setCurrentPage]=useState(1)
+  const currentPageProductLastIndex=currentPage*productsPerPage
+  const currentPageProductFirstIndex=currentPageProductLastIndex-productsPerPage
+
+  const visibleProduct=allproducts?.slice(currentPageProductFirstIndex,currentPageProductLastIndex)
+
+  const navigatePrevPage=()=>{
+    if(currentPage!=1){
+      setCurrentPage(currentPage-1)
+    }
+
+    
+
+  }
+  const navigateNextPage=()=>{
+    if(currentPage!=totalPages){
+      setCurrentPage(currentPage+1)
+    }
+
+  }
 
   useEffect(() => {
     if (allproducts.length === 0) {
       dispatch(fetchAllProducts())
     }
   }, [])
+  useEffect(() => {
+  setCurrentPage(1)
+}, [allproducts])
 
   return (
     <>
@@ -23,7 +51,7 @@ function Home() {
       ) : (
         <div className='pt-36 mx-5'>
           <div className='grid grid-cols-4 gap-4'>
-            {allproducts?.length > 0 ? allproducts?.map((product) => (
+            {allproducts?.length > 0 ? visibleProduct?.map((product) => (
               <div key={product?.id} className='rounded p-2 shadow flex flex-col h-full'>
                 <div className='flex justify-center items-center h-48'>
                   <img
@@ -41,6 +69,18 @@ function Home() {
               </div>
             )) : <p>No Products Found</p>}
           </div>
+
+          {/* Pagination */}
+          <div className='text-center mt-20 font-bold text-2xl'>
+            <button onClick={navigatePrevPage}  className='cursor-pointer'>
+              <FontAwesomeIcon icon={faBackward} />
+            </button>
+            <span className='ms-3 me-3'>{currentPage} of {totalPages}</span>
+            <button onClick={navigateNextPage} className='cursor-pointer'>
+              <FontAwesomeIcon icon={faForward} />
+            </button>
+          </div>
+          
         </div>
       )}
     </>
